76aee1238fb6304d30d9fb105f9fcfd61ac43e9f
Shalini <shalini@nagarro.com>
2022-08-02 16:33:21 +0530

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback +=
+    (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            var signalRHelper = new SignalRHelper();
+            signalRHelper.Start();
+
+            Console.Read();
+        }
+    }
+}
